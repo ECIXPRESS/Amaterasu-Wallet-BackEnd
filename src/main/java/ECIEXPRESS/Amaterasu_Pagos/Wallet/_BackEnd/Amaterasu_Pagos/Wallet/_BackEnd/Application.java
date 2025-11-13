bff736276456417bca3875fdf925d5e1023987b9
tomasote572 <tomas.ramirez@mail.escuelaing.edu.co>
2025-11-13 17:21:09 -0500

    Repositorio con tecnologias

---
+++ src/main/java/ECIEXPRESS/Amaterasu_Pagos/Wallet/_BackEnd/Amaterasu_Pagos/Wallet/_BackEnd/Application.java
@@ -0,0 +1,13 @@
+package ECIEXPRESS.Amaterasu_Pagos.Wallet._BackEnd.Amaterasu_Pagos.Wallet._BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/ECIEXPRESS/Amaterasu_Pagos/Wallet/_BackEnd/Amaterasu_Pagos/Wallet/_BackEnd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ECIEXPRESS.Amaterasu_Pagos.Wallet._BackEnd.Amaterasu_Pagos.Wallet._BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
