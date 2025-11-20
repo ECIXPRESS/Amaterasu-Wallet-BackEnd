5d4d8ba78c1402f95f6f44ec48532e02a4a961d6
tomasote572 <tomas.ramirez@mail.escuelaing.edu.co>
2025-11-19 21:33:33 -0500

    Creacion del servicio completo con pruebas

---
+++ src/main/java/edu/dosw/AmaterasuWalletBackEnd/AmaterasuWalletBack/AmaterasuWalletBackApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.AmaterasuWalletBackEnd.AmaterasuWalletBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmaterasuWalletBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmaterasuWalletBackApplication.class, args);
+	}
+
+}
