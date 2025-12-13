9ea51a725c8950a67e09a9a1bc37d5da754b59ab
komi_yes <danielpalacios04@gmail.com>
2025-12-12 23:03:09 -0500

    refactor(config): Update OpenAPI and server port
    Update OpenAPI configuration to include server URL.
    Change server port from 8080 to 8083 in application.yml.

--- src/main/java/edu/dosw/AmaterasuWalletBackEnd/AmaterasuWalletBack/Config/OpenApiConfig.java
+++ src/main/java/edu/dosw/AmaterasuWalletBackEnd/AmaterasuWalletBack/Config/OpenApiConfig.java
@@ -31,7 +31,12 @@ public class OpenApiConfig {
                         .description("API REST para la gestión del monedero virtual de los usuarios en el sistema ECIEXPRESS. "
                                 + "Este microservicio administra el saldo del usuario, recargas, "
                                 + "movimientos financieros y validaciones de disponibilidad de fondos.")
-                        .version("1.0.0"));
+                        .version("1.0.0")).
+                servers(List.of(
+                        new Server()
+                                .url("http://localhost:8083")
+                                .description("Servidor de Desarrollo")
+                ));
     }
 }
 
